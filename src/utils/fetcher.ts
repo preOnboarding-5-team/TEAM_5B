2a0b21ef319ce9488d341ade96c3ca4b21238266
Moon-Ga <dev.jaeseok@gmail.com>
2022-05-22 11:19:18 +0900

    deplay

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -6,7 +6,7 @@ const fetcher = async (searchText: string) => {
   // 배포시 수정 필요 (http://apis.data.go.kr/)
   try {
     const res = await axios.get<IGetDissRes>(
-      `/proxy/B551182/diseaseInfoService/getDissNameCodeList`,
+      `/B551182/diseaseInfoService/getDissNameCodeList`,
       {
         params: {
           _type: 'json',
